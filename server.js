83e82e0790eabddd896206830719400e837784ea
mhendricks96 <mhendricks96@yahoo.com>
2021-04-10 15:11:11 -0700

    completed basic server setup

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 //console.log('yea, mike');
 
+const { response } = require('express');
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
 //This is where most of my code will go
+
+app.get('/',(request, response) => {
+  response.send('chea!');
+});
 
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
